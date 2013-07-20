ac183979a4aa91f4a7e31e698c6ac31bdbb1e3a2
atanas.tsankov <atanas_tsankov_91@abv.bg>
2013-07-20 03:11:54 +0300

    test commit through eclipse

--- NFitness/src/com/atanas/tsankov/constants/Constants.java
+++ NFitness/src/com/atanas/tsankov/constants/Constants.java
@@ -2,6 +2,7 @@ package com.atanas.tsankov.constants;
 
 public class Constants {
 
+	//Constants
 	public static final String NAME = "name";
 	public static final String PROGRAMME_FILE = "programme.txt";
 }
